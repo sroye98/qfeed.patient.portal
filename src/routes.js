1b6a5b38d21a3ba6a0b595469ababd3d5c5ac912
Saachi Roye <saachi.roye@gmail.com>
2021-07-16 17:52:35 -0500

    added 404 page

--- src/routes.js
+++ src/routes.js
@@ -2,12 +2,14 @@ import { Route, Switch } from 'react-router-dom';
 
 import StepNavigation from './views/Steps';
 import Confirmation from './views/Confirmation';
+import NotFound from './views/404';
 
 const Routes = (props) => {
     return (
         <Switch>
             <Route exact path="/" component={StepNavigation} />
             <Route exact path="/confirmation" component={Confirmation} />
+            <Route path="*" component={NotFound} />
         </Switch>
     );
 };
